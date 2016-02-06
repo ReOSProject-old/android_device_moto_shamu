c263721189f783855e91dd65332067d2e249d2ea
Shawn W <wahi.shawn@gmail.com>
2016-02-05 23:03:29 -0800

    aosp_shamu => re_shamu

--- re_shamu.mk
+++ re_shamu.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+#Inherit ReOS common configuration
+$(call inherit-product-if-exists, vendor/re/main.mk)
+
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
@@ -23,17 +26,15 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := aosp_shamu
+PRODUCT_NAME := re_shamu
 PRODUCT_DEVICE := shamu
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Shamu
+PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_RESTRICT_VENDOR_FILES := true
 
 $(call inherit-product, device/moto/shamu/device.mk)
 $(call inherit-product-if-exists, vendor/moto/shamu/device-vendor.mk)
-
-PRODUCT_NAME := aosp_shamu
 
 PRODUCT_PACKAGES += \
     Launcher3
